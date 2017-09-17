874eff37532f0b055a69aacf4b62503f53b83c48
jojobeth1@gmail.com <jojobeth1@gmail.com>
2017-09-17 13:13:04 -0700

    user validations done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,10 +5,27 @@ class UsersController < ApplicationController
     @users = User.all
     render :index
   end
-  
+
   def create
-    @user = User.create(user_params)
-    redirect_to @user
+    user_params = params.require(:user).permit(:username, :email, :password)
+    user_params[:username] = user_params[:username].downcase
+    user_params[:email] = user_params[:email].downcase
+
+    if User.find_by username: user_params[:username]
+    # Is username already in db?
+      flash[:error] = "Username taken. Please try again."
+      redirect_to root_path
+    elsif User.find_by email: user_params[:email]
+      # Is email already in db?
+      flash[:error] = "Email taken. Please try again."
+      redirect_to root_path
+    elsif user_params[:password].length < 8
+      flash[:error] = 'Password must be at least 8 characters long.'
+      redirect_to root_path
+    else
+      @user = User.create(user_params)
+      redirect_to user_path(@user.username)
+    end
   end
 
   def show
@@ -17,10 +34,10 @@ class UsersController < ApplicationController
   end
 
 
-  private
-
-  def user_params
-    params.require(:user).permit(:username, :email, :password_digest)
-  end
+  # private
+  #
+  # def user_params
+  #   params.require(:user).permit(:username, :email, :password_digest)
+  # end
 
 end
